d9daa40c513403e7416abe0c55d5be30c83ac527
ChristianAguero <christian.aguero.aguero@potros.itson.edu.mx>
2022-11-24 17:59:25 -0700

    Creada conexion

--- src/mx/itson/jijijija/persistencia/Conexion.java
+++ src/mx/itson/jijijija/persistencia/Conexion.java
@@ -4,10 +4,35 @@
  */
 package mx.itson.jijijija.persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author shiri
  */
 public class Conexion {
+    
+    /**
+     *  Sirve para crear la conexion entre el programa y la base de datos
+     * @return La conexion hacia la base de datos
+     */
+    public static Connection obtener(){
+        
+        Connection conexion = null;
+        
+        try{
+            
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost/jijijijadb?user=root&password=");
+            
+        }catch(Exception ex){
+            
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+            
+        }
+        
+        return conexion;
+        
+    }
     
 }
